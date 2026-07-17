3a4a514a23a9e67995ad8df0a2502433a76fcf43
shurmeri <sakari.j.hurmerinta@stundent.jyu.fi>
2026-07-17 14:30:15 +0300

    lisätty google autentikointi testipohja

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+
+GOOGLE_CLIENT_ID = "876367506780-dd0g3bsb32mtujob4f25i5rm2fiatkpo.apps.googleusercontent.com"
+GOOGLE_CLIENT_SECRET = "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"
--- vt4.py
+++ vt4.py
@@ -1,16 +1,46 @@
-import os
-
-from flask import Flask
+from flask import Flask, url_for, session
+from flask import render_template, redirect
+from authlib.integrations.flask_client import OAuth
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.secret_key = '!secret'
+app.config.from_object('config')
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
+
+CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
+oauth = OAuth(app)
+oauth.register(
+    name='google',
+    server_metadata_url=CONF_URL,
+    client_kwargs={
+        'scope': 'openid email profile',
+        'prompt': 'consent'
+    }
+)
 
 
-@app.route("/")
-def hello_world():
-    """Example Hello World route."""
-    name = os.environ.get("NAME", "World")
-    return f"Hello {name}!"
+@app.route('/')
+def homepage():
+    user = session.get('user')
+    return render_template('home.html', user=user)
 
 
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+@app.route('/login')
+def login():
+    redirect_uri = url_for('auth', _external=True)
+    return oauth.google.authorize_redirect(redirect_uri)
+
+
+@app.route('/auth')
+def auth():
+    token = oauth.google.authorize_access_token()
+    session['user'] = token['userinfo']
+    email = session['user']['email']
+    return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
